69fdaa58688525b9cd37eaa095329e170733bd65
Maryia <simonna13@bk.ru>
2022-05-17 08:21:08 +0300

    4

--- academyy/src/by/academy/homework6/Task4.java
+++ academyy/src/by/academy/homework6/Task4.java
@@ -1,8 +1,10 @@
 package by.academy.homework6;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,58 +16,65 @@ import java.util.Random;
 //сохраняем в наш файл. Создайте файл result.txt и запишите туда список всех файлов и их размеры.
 
 public class Task4 {
-	 public static final Random RANDOM = new Random();
-	 
-	    public static void main(String[] arg) throws IOException {
-	        File dirs = new File("academyy/src/by/academy/homework6//Files");
-	 
-	        if (!dirs.exists()) {
-	            dirs.mkdirs();
-	        }
-	 
-	        for (int i = 1; i < 101; i++) {
-	            File file = new File(dirs, i + ".txt");
-	 
-	            if (!file.exists()) {
-	 
-	                file.createNewFile();
-	            }
-	        }
-	        for (int i = 1; i < 101; i++) {
-	            String path = "academyy//src//by//academy//homework6//Files" + i + ".txt";
-	            try (InputStream input = new FileInputStream("Task2.txt");
-	                    OutputStream output = new FileOutputStream(path)) {
-	                int size = input.available();
-	                System.out.print(size);
-	               int size2 = RANDOM.nextInt(size);
-	               char a = (char) input.read();
-	               String text = Character.toString(a);
-	               for (int j = 0; j < size2; j++) {
-	                char[] x = text.substring(0, j).toCharArray();
-	                 output.write((char) x[j]);
-	                }
-	               input.close();
-	               output.close();
-	            } catch (IOException e) {
-	                System.err.println(e.getMessage());
-	            }
-	            
-	            try (OutputStream output2 = new FileOutputStream("result_info.txt")) {
-	            for (File file : dirs.listFiles()) {
-	            	String info = "Имя файла: " + file.getName()+"Размер: " + file.length() + " байт.";
-	            	char[]x = info.toCharArray();
-	            	for (int j = 1; j < 101; j++) {
-	            	output2.write(x[j]);
-	            	}
-	            	output2.close();
-	            	
-	            } }catch (IOException e) {
-	                System.err.println(e.getMessage());
-	        }
-	 
-	        }
-	    }
+	public static final Random RANDOM = new Random();
+
+	public static void main(String[] arg) throws IOException {
+		File dirs = new File("academyy/src/by/academy/homework6//Files");
+
+		if (!dirs.exists()) {
+			dirs.mkdirs();
+		}
+
+		for (int i = 1; i < 101; i++) {
+			File file = new File(dirs, i + ".txt");
+
+			if (!file.exists()) {
+
+				file.createNewFile();
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new FileReader("Task2.txt"))) {
+			String i = null;
+			while ((i = br.readLine()) != null) {
+				sb.append(i);
+			}
+
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		String text = sb.toString();
+
+		for (int i = 1; i < 101; i++) {
+			String path = "academyy//src//by//academy//homework6//Files" + i + ".txt";
+			try (OutputStream output = new FileOutputStream(path)) {
+			
+				int size2 = RANDOM.nextInt(text.length());
+				
+				char[] x = text.substring(0, size2).toCharArray();
+				for (int j = 0; j < size2; j++) {
+					output.write((char) x[j]);
+				}
+				output.close();
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+
+			try (OutputStream output2 = new FileOutputStream("result_info.txt")) {
+				for (File file : dirs.listFiles()) {
+					String info = "Имя файла: " + file.getName() + "Размер: " + file.length() + " байт.";
+					char[] x = info.toCharArray();
+					for (int j = 1; j < 101; j++) {
+						output2.write(x[j]);
+					}
+					output2.close();
+
+				}
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+
+		}
+	}
 }
-	 
-	
-	    
